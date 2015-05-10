8bcca6851f50e443948680e448366c93563bab08
zhenghao175 <282744685@qq.com>
2015-05-10 19:48:52 +0800

    增加了注册，数据库建表
    由于5.1放假和别的项目一些事情，很久没有进展，上次课结束本来想去掉注册和登陆，后来想想放着就放着吧，没准什么时候会写服务端，这次完成了注册功能，并学习了SQlite的用法，建表操作，这段时间会加快进程，争取抓紧完成。

--- classmanagersystem/gen/com/example/classmanagersystem/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.classmanagersystem;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- classmanagersystem/src/com/example/classmanagersystem/MainActivity.java
+++ classmanagersystem/src/com/example/classmanagersystem/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.classmanagersystem;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.example.classmanagersystem.MainActivity;
+import com.example.classmanagersystem.R;
+import com.example.classmanagersystem.helper.MySqlHelper;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -20,6 +31,8 @@ public class MainActivity extends Activity {
 	private Button btn_register;
 	private Button btn_login;
 	private Button btn_exit;
+	private MySqlHelper mySqlHelper;
+	private SQLiteDatabase db;
 
 	private List<String> lists = new ArrayList<String>();
 
@@ -27,8 +40,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mySqlHelper = new MySqlHelper(MainActivity.this, "student_inf.db",
+				null, 1);
 		
+		db = mySqlHelper.getWritableDatabase();
+		btn_register = (Button) findViewById(R.id.register);
 		btn_login = (Button) findViewById(R.id.login);
+		btn_exit = (Button) findViewById(R.id.exit);
+		btn_register.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				showMyDialog();
+			}
+		});
+		
 		
 		btn_login.setOnClickListener(new OnClickListener() {
 
@@ -41,7 +68,65 @@ public class MainActivity extends Activity {
 	}
 
 	//注册
-	
+	public void showMyDialog() {
+		Builder builder = new Builder(MainActivity.this);
+		LayoutInflater inflater = LayoutInflater.from(MainActivity.this);
+		View view = inflater.inflate(R.layout.registerlayout, null);
+		Button btn_confirm = (Button) view.findViewById(R.id.btn_confirm);
+		Button btn_back = (Button) view.findViewById(R.id.btn_back);
+		final EditText editUserName = (EditText) view
+				.findViewById(R.id.editText1);
+		final EditText editPswd = (EditText) view.findViewById(R.id.editText2);
+		final EditText editPswd_confirm = (EditText) view
+				.findViewById(R.id.editText3);
+		final AlertDialog dialog = builder.setTitle("学生信息注册").setView(view)
+				.create();
+
+		btn_confirm.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if ((editPswd.getText().toString()).equals((editPswd_confirm
+						.getText().toString()))) {
+					Cursor cursor = db.rawQuery(
+							"select count(*) from user where username = '"
+									+ editUserName.getText().toString() + "'",
+							null);
+					cursor.moveToNext();
+					int count = cursor.getInt(0);
+					if (count == 0) {
+						ContentValues values = new ContentValues();
+						values.put("username", editUserName.getText()
+								.toString());
+						
+						db.insert("user", null, values);
+						dialog.dismiss();
+						Toast.makeText(MainActivity.this, "注册成功！",
+								Toast.LENGTH_SHORT).show();
+					} else {
+						Toast.makeText(MainActivity.this, "您注册的用户名已存在！",
+								Toast.LENGTH_SHORT).show();
+					}
+					cursor.close();
+				} else {
+					Toast.makeText(MainActivity.this, "您两次输入的密码不一样！",
+							Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+
+		dialog.show();
+		btn_back.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				dialog.dismiss();
+			}
+		});
+
+	}
 	//登录
 	public void login() {
 
